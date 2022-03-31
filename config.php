86e6d1a78493082124d024077111174085406f67
Oleg Fil <abyzswalker@gmail.com>
2022-03-31 12:01:49 +0300

    .

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = [
+    'db' => [
+        'server' => 'spa-db',
+        'dbname' => 'spa',
+        'username' => 'dbadmin',
+        'password' => '123456',
+        'charset' => 'utf8'
+    ]
+];
