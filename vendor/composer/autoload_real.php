0ebf53c146e9ae6c6c433d4c003f363d009ca39a
Oleg Fil <abyzswalker@gmail.com>
2023-06-02 02:36:12 +0300

    test

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit5c72ba020e2263e6bf3345a7671b6cee
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit5c72ba020e2263e6bf3345a7671b6cee', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit5c72ba020e2263e6bf3345a7671b6cee', 'loadClassLoader'));
