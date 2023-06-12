b002a8efc15812a4beea0c16b8651aa2aae88803
o.fil <abyzswalker@gmail.com>
2023-06-12 10:23:54 +0300

    test

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -86,8 +86,8 @@ class ComposerStaticInit5c72ba020e2263e6bf3345a7671b6cee
         'phpDocumentor\\Reflection\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
         ),
         'XdgBaseDir\\' => 
         array (
